05b204994343ab1a9236e3074707eeadc80a57b6
Jessica-Margret004 <146917155+Jessica-Margret004@users.noreply.github.com>
2025-05-07 17:43:23 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -5,14 +5,8 @@ import { getAnalytics } from "firebase/analytics";
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import AsyncStorage from '@react-native-async-storage/async-storage'; // Import AsyncStorage
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAfqEUTS3ZaIdxlQMDeGFgDV2ZFPi1zYr4",
-  authDomain: "my-guardian-58485.firebaseapp.com",
-  projectId: "my-guardian-58485",
-  storageBucket: "my-guardian-58485.firebasestorage.app",
-  messagingSenderId: "984824841547",
-  appId: "1:984824841547:web:9efd8a2e924e36f32f54ce",
-  measurementId: "G-MCJQF0F7SD"
+//const firebaseConfig = {
+
 };
 
 const app = initializeApp(firebaseConfig);
